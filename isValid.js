e0d44f6be47de4f0c2a12a694cc086dea83de40a
deminvv1 <deminvladtema@mail.ru>
2025-09-19 12:18:16 +0300

    task_1_3_isValidFunction

---
+++ isValid.js
@@ -0,0 +1,19 @@
+function isValid(board, row, col, num) {
+  for (let i = 0; i < 9; i++) {
+    if (i !== col && board[row][i] === num) return false;
+    if (i !== row && board[i][col] === num) return false;
+  }
+
+  const r0 = Math.floor(row / 3) * 3;
+  const c0 = Math.floor(col / 3) * 3;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      const rr = r0 + i,
+        cc = c0 + j;
+      if ((rr !== row || cc !== col) && board[rr][cc] === num) return false;
+    }
+  }
+  return true;
+}
+
+module.exports = isValid;
