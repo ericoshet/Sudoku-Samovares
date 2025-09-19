499969aae999a106098fdd0eb87529e44adb04cb
Rakiiia <Rakiiia@mail.ru>
2025-09-19 11:49:51 +0300

    function_readFile_done

--- sudoku.js
+++ sudoku.js
@@ -1,9 +1,25 @@
 
 const fs=require('fs')
 
-function read(path) {
-  const readFile = fs.readFileSync(path, 'utf-8')
-  return readFile;
+
+function readAll(path) {
+  const lines = fs.readFileSync(path, 'utf-8').trim().split('\n');  // Добавить EOL
+
+  return lines.map(line => {
+    const vals = [...line]
+      .filter(ch => /[{0-9}.\-]/.test(ch))
+      .map(ch => (ch === '-' || ch === '.' ? 0 : Number(ch)));
+
+    if (vals.length !== 81) {
+      throw new Error('Строка паззла должна содержать 81 символ (цифры и "-" / ".")');
+    }
+
+    const board = [];
+    for (let r = 0; r < 9; r++) {
+      board.push(vals.slice(r * 9, r * 9 + 9));
+    }
+    return board;
+  });
 }
 
 
