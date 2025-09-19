608b040a7c4ad4bdc0037efedfc69a94e0c2fe6d
Rakiiia <Rakiiia@mail.ru>
2025-09-19 15:44:48 +0300

    wtf2

--- prettyBoard.js
+++ prettyBoard.js
@@ -1,13 +1,14 @@
 const readAll = require('./sudoku.js');
 const { solve, isSolved } = require('./isSolve');
+const colors = require('colors')
 
 function prettyBoard(board) {
-  const sep = '------+-------+------';
+  const sep = '------+-------+------'.rainbow;
   for (let i = 0; i < 9; i++) {
     let line = '';
     for (let j = 0; j < 9; j++) {
-      line += `${board[i][j] || '.'} `;
-      if ((j + 1) % 3 === 0 && j < 8) line += '| ';
+      line += `${board[i][j] || '.'.blue.bold} `;
+      if ((j + 1) % 3 === 0 && j < 8) line += '| '.rainbow;
     }
     console.log(line.trim());
     if ((i + 1) % 3 === 0 && i < 8) console.log(sep);
@@ -16,12 +17,12 @@ function prettyBoard(board) {
 
 const boards = readAll('./puzzles.txt');
 boards.forEach((board, i) => {
-  console.log(`\nСудоку номер ${i + 1}:`);
+  console.log(`\nСудоку номер ${i + 1}:`.green);
   prettyBoard(board);
-  console.log('Решение:');
+  console.log('Решение:'.blue);
   if (solve(board)) {
     prettyBoard(board);
-    console.log('Решено?', isSolved(board));
+    console.log('Решено?'.red, isSolved(board));
   } else {
     console.log('Решение не найдено');
   }
