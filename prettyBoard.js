d8d8503bf1b6b3208ed3966600b59e3e192ff28a
NadyaGurhan <nadi.gavina98@mail.ru>
2025-09-19 13:43:11 +0300

    release prettiAndPunner

---
+++ prettyBoard.js
@@ -0,0 +1,29 @@
+const readAll = require('./sudoku.js');
+
+function prettyBoard(board) {
+  const sep = '------+-------+------';
+  for (let i = 0; i < 9; i++) {
+    let line = '';
+    for (let j = 0; j < 9; j++) {
+      line += `${board[i][j] || '.'} `;
+      if ((j + 1) % 3 === 0 && j < 8) line += '| ';
+    }
+    console.log(line.trim());
+    if ((i + 1) % 3 === 0 && i < 8) console.log(sep);
+  }
+}
+
+const boards = readAll('./puzzles.txt');
+boards.forEach((board, i) => {
+  console.log(`\nСудоку номер ${i + 1}:`);
+  prettyBoard(board);
+  console.log('Решение:');
+  if (solve(board)) {
+    prettyBoard(board);
+    console.log('Решено?', isSolved(board));
+  } else {
+    console.log('Решение не найдено');
+  }
+});
+
+module.exports = { prettyBoard };
